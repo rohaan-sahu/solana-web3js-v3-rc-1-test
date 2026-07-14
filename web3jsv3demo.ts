8bac7cace20c6edd04c0f8cebf11d206599ab7b9
rohaan-sahu <94603789+rohaan-sahu@users.noreply.github.com>
2026-07-14 18:24:22 +0530

    prettify 'web3jsv3demo.ts'

--- web3jsv3demo.ts
+++ web3jsv3demo.ts
@@ -1,4 +1,12 @@
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
+import { 
+        clusterApiUrl,
+        Connection,
+        Keypair,
+        LAMPORTS_PER_SOL,
+        sendAndConfirmTransaction,
+        SystemProgram,
+        Transaction
+} from "@solana/web3.js";
 import { readFileSync } from "node:fs";
 
 (async () => { 
