57d5496c22aa39b12805f9212bf97d2bf86c9260
rohaan-sahu <rbbsr8292@gmail.com>
2026-07-14 12:47:18 +0000

    System program transfer
    Committed the keys deliberatly. They have some devnet SOL & some transactions on them

---
+++ web3jsv3demo.ts
@@ -0,0 +1,43 @@
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
+import { readFileSync } from "node:fs";
+
+(async () => { 
+        const connection = new Connection(clusterApiUrl("devnet"));
+
+        const signerFileKeyBuffer = JSON.parse(readFileSync(
+            "./keys/w3rpvueukHhmAmzcwYkU1tir1ufJEEUoKRUoX497odD.json",
+            'utf-8'
+        ));
+        const signer = await Keypair.fromSecretKey(signerFileKeyBuffer);
+
+        console.log(`\nsigner address: ${signer.address}\n`);
+
+        const receiverFileKeyBuffer = JSON.parse(readFileSync(
+            "./keys/r3d5VmSy3JKoWjdcym1KP9MPxKe7eMWCYVHHBRA4Nws.json",
+            'utf-8'
+        ));
+
+        const receiver = await Keypair.fromSecretKey(receiverFileKeyBuffer);
+
+        console.log(`\nreceiver address: ${receiver.address}\n`);
+        
+        const ix = SystemProgram.transfer({
+            fromPubkey: receiver.publicKey,
+            toPubkey: signer.publicKey,
+            lamports: 0.1*LAMPORTS_PER_SOL,
+        });
+
+        const tx = new Transaction();
+        tx.add(ix);
+
+        const sx = await sendAndConfirmTransaction(
+            connection,
+            tx,
+            [receiver],
+            {commitment: 'confirmed'}
+        );
+
+        console.log(`signature: ${sx}`);
+
+    }
+)()
